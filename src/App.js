e84461ab53cfc887f186df6d8ff40e070fd843eb
unknown <parchamonika@gmail.com>
2021-09-18 02:10:22 -0700

    Add: task

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import "./App.css";
+import { getResultOfClass, getBestThreeStudents } from "./task";
+import { classes } from "./utills/mockdata";
+
+function App() {
+  const results = Object.values(classes).map((data) => {
+    const classResult = getResultOfClass(data);
+    return {
+      className: data,
+      result: classResult,
+    };
+  });
+
+  const topThree = getBestThreeStudents();
+
+  return (
+    <div className="App">
+      <div>
+        <ul>
+          <h1> Top Three Students Are </h1>
+          {topThree.map((data, index) => {
+            return (
+              <li key={`top_${index}`}>
+                {data.name.first + data.name.last} with {data.total} marks
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      {results.map((data, index) => {
+        return (
+          <div key={index}>
+            <h3> Result of class {data.className} </h3>
+            <table style={{ width: "100%" }}>
+              <thead>
+                <tr>
+                  <td>Name </td> <td colSpan={3}>subjects </td>
+                </tr>
+              </thead>
+              <tbody>
+                {data.result.map((res, index) => {
+                  return (
+                    <tr key={`result_${index}`}>
+                      <td>{res.name.first + res.name.last} </td>
+                      {res.marks.map((data, index) => {
+                        return (
+                          <table
+                            style={{ width: "100%" }}
+                            key={`marks_${index}`}
+                          >
+                            <tbody>
+                              <tr className="innerTr">
+                                <td style={{ width: "33%" }}>{data.subject}</td>
+                                <td style={{ width: "33%" }}>{data.marks}</td>
+                                <td style={{ width: "33%" }}>{data.status}</td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        );
+                      })}
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/task/index.js
@@ -0,0 +1,48 @@
+import { studentsData, minimumMarks } from "../utills/mockdata";
+
+export const getBestThreeStudents = () => {
+  const totalMarks = getTotalMarks(studentsData);
+  return totalMarks.sort((a, b) => b.total - a.total).slice(0, 3);
+};
+
+export const getTotalMarks = (studentsData) => {
+  const totalMarks = studentsData.map((data) => {
+    const total = data.marks.reduce((acc, curr) => {
+      return acc.marks > minimumMarks
+        ? { marks: acc.marks + curr.marks }
+        : { marks: curr.marks };
+    });
+    return {
+      ...data,
+      total: total.marks,
+    };
+  });
+  return totalMarks;
+};
+
+export const getStudentStatus = (marks) => {
+  const marksWithStatus = marks.map((data) => {
+    return {
+      ...data,
+      status: data.marks > minimumMarks ? "Pass" : "Fail",
+    };
+  });
+
+  return marksWithStatus;
+};
+
+export const getResultOfClass = (className) => {
+  const totalMarks = getTotalMarks(studentsData);
+  const result = totalMarks
+    .filter((data) => {
+      return data.class === className;
+    })
+    .map((data) => {
+      const marksWithStatus = getStudentStatus(data.marks);
+      return {
+        ...data,
+        marks: marksWithStatus,
+      };
+    });
+  return result.sort((a, b) => b.total - a.total);
+};
